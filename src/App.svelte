64f7f492f27d1521569c9019c4afaf0add75d294
Hakan Alpay <hak7alp@gmail.com>
2022-06-28 20:28:16 -0700

    ya

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,7 @@
 
     // for mobile share copy link
     if (!youtubeID) {
-      let youtubeID = youtubeLink.match(/(?<=youtu\.be\/).*/);
+      youtubeID = youtubeLink.match(/(?<=youtu\.be\/).*/);
     }
 
     // TODO: shorts
